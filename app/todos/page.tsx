8050b74ff41b0d00b1a0c23af9cc0bbd3473cbf9
Ernesto Osuna <ernesto.osuna@unosquare.com>
2024-11-07 13:43:50 -0600

    finish mutation

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -2,7 +2,7 @@ import NewTodoForm from '@/components/NewTodoForm'
 import TodoList from '@/components/TodoList'
 import db from '@/utils/db/main'
 const getData = async () => {
-   return await db.todo.findMany({})
+   return await db.todo.findMany()
 }
 
 export default async function Todos() {
--- components/Todo.tsx
+++ components/Todo.tsx
@@ -6,7 +6,12 @@ import { useTransition } from "react";
 export default function TodoComponent({todo}: {todo: Todo}) {
     const [isPending, startTransition] = useTransition()
     
-    return <div onClick={() => startTransition(() => {completeTodo(todo.id)})}>
+    return <div 
+        className={`border border-black/20  ${todo.completed?'line-through text-gray-700 cursor-not-allowed':'cursor-pointer'}`}
+        onClick={() => startTransition(() => {
+        completeTodo(todo.id)
+        })
+    }>
         {todo.content}
     </div>
 }
